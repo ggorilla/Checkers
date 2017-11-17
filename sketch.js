56de34dbfd011d84905d30be26f1bfa34963d846
Gilberto Hernandez <gilbertoh56050@students.laalliance.org>
2017-11-17 17:53:32 +0000

    First Commit

---
+++ sketch.js
@@ -0,0 +1,66 @@
+function setup() {
+  createCanvas(451,451)
+}
+
+function draw() {
+  background(0)
+  fill(250  )
+
+  var x = 50
+  var y = 50
+  
+
+  for (var i = 0; i < 8; i++) {
+    rect(x, y, 50, 50)
+    x += 50
+  }
+  
+  x = 50
+  y += 50
+  for (var i = 0; i < 8; i++) {
+    rect(x, y, 50, 50)
+    x += 50
+  }
+  
+  x = 50
+  y += 50
+  for (var i = 0; i < 8; i++) {
+    rect(x, y, 50, 50)
+    x += 50
+  }
+  
+  x = 50
+  y += 50
+  for (var i = 0; i < 8; i++) {
+    rect(x, y, 50, 50)
+    x += 50
+  }
+  
+  x = 50
+  y += 50
+  for (var i = 0; i < 8; i++) {
+    rect(x, y, 50, 50)
+    x += 50
+  }
+  
+  x = 50
+  y += 50
+  for (var i = 0; i < 8; i++) {
+    rect(x, y, 50, 50)
+    x += 50
+  }
+  
+  x = 50
+  y += 50
+  for (var i = 0; i < 8; i++) {
+    rect(x, y, 50, 50)
+    x += 50
+  }
+  
+  x = 50
+  y += 50
+  for (var i = 0; i < 8; i++) {
+    rect(x, y, 50, 50)
+    x += 50
+  }
+}
